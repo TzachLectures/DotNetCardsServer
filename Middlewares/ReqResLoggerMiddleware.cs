39a2c472aa2e8ae26103954738ebd01d34ab6900
Benny Dabush <bennydabush@gmail.com>
2024-01-15 19:01:35 +0200

    add custom logger

--- Middlewares/ReqResLoggerMiddleware.cs
+++ Middlewares/ReqResLoggerMiddleware.cs
@@ -1,4 +1,6 @@
 ﻿using Amazon.Runtime.Internal;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Diagnostics;
 
 namespace DotNetCardsServer.Middlewares
 {
@@ -13,21 +15,31 @@ namespace DotNetCardsServer.Middlewares
 
         public async Task InvokeAsync(HttpContext context)
         {
+            Stopwatch watch = Stopwatch.StartNew();
+
             //do something with the request
             string path = context.Request.Path;
             string method = context.Request.Method;
-            string referer = context.Request.Headers["Referer"].ToString();
-            var origin = context.Request.Headers["Origin"].ToString();
-
-            Console.WriteLine($"we got new request from referer {referer} to {path} with method {method}");
-
+            string origin = context.Request.Headers["origin"].FirstOrDefault();
+            string timeStamp = "["+DateTime.Now.ToString()+"]";
             await _next(context);
-
             ////do something with the response (after it sent)
-            string statusCode = context.Response.StatusCode.ToString();
-            Console.WriteLine("We sent back response with status " + statusCode);
+             watch.Stop();
+            long responseTime = watch.ElapsedMilliseconds;
+            int statusCode = context.Response.StatusCode;
 
-        }
+            if (statusCode >= 400)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine($"{timeStamp} incoming request from {origin} {path} {method} {statusCode} - {responseTime}ms");
+            Console.ResetColor();
+        } // date time - path - method - status code - response time 
+        //ex: [15/01/2024 18:28:35] incoming request from {origin} /api/users GET 200 - 10ms 
 
 
     }
